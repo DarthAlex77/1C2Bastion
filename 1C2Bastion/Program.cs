d68a405c32fe8cde572b32abfb8f7f6f900312a0
Aleksey Surgaev <Alex.Surgaev@gmail.com>
2024-02-13 21:03:56 +0300

    Add project files.

---
+++ 1C2Bastion/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Serialization;
+using ExcelDataReader;
+
+namespace _1C2Bastion
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Файл .xls не найден, перетащите его на значок программы");
+                Console.WriteLine("Нажмите любую кнопку для выхода");
+                Console.Read();
+                return;
+            }
+            Document doc = new Document();
+
+            using (FileStream stream = File.Open(args[0], FileMode.Open, FileAccess.Read))
+            {
+                using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    DataSet result = reader.AsDataSet();
+                    result.Tables[0].Rows[0].Delete();
+                    result.AcceptChanges();
+                    foreach (DataRow dataRow in result.Tables[0].Rows)
+                    {
+                        Row row = new Row();
+                        row.PassType      = 1.ToString();
+                        row.Name          = dataRow.ItemArray[0].ToString();
+                        row.FirstName     = dataRow.ItemArray[1].ToString();
+                        row.SecondName    = dataRow.ItemArray[2].ToString();
+                        row.TableNo       = dataRow.ItemArray[3].ToString();
+                        row.Birthdate     = dataRow.ItemArray[4].ToString();
+                        row.Sex           = dataRow.ItemArray[5].ToString() == "Женский" ? "1" : "0";
+                        row.Address       = dataRow.ItemArray[6].ToString();
+                        row.Citizenship   = dataRow.ItemArray[7].ToString();
+                        row.DocType       = dataRow.ItemArray[8].ToString();
+                        row.DocSer        = dataRow.ItemArray[9].ToString();
+                        row.DocNo         = dataRow.ItemArray[10].ToString();
+                        row.DocIssueDate  = dataRow.ItemArray[11].ToString();
+                        row.DocIssueOrgan = dataRow.ItemArray[12].ToString();
+                        string[] split = Regex.Split(dataRow.ItemArray[13].ToString(), @"(?<!^)(?=[А-Я])");
+                        if (split.Length == 1)
+                        {
+                            row.Department = split[0];
+                        }
+                        else
+                        {
+                            row.Department = split[0].Trim() + @" \ " + split[1].Trim();
+                        }
+                        row.Post       = dataRow.ItemArray[14].ToString();
+                        row.Birthplace = dataRow.ItemArray[15].ToString();
+                        doc.Rows.Add(row);
+                    }
+                }
+            }
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent             = true;
+            settings.Encoding           = Encoding.GetEncoding("WINDOWS-1251");
+            XmlSerializer xml   = new XmlSerializer(typeof(Document));
+            string        path  = Path.GetFullPath(args[0]).Split('.')[0] + ".xml";
+            using (XmlWriter writer = XmlWriter.Create(path, settings))
+            {
+                xml.Serialize(writer, doc);
+            }
+        }
+    }
+}
---
+++ 1C2Bastion/Row.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace _1C2Bastion
+{
+    [XmlRoot(ElementName = "Document")]
+    public class Document
+    {
+        [XmlElement(ElementName = "ROW")]
+        public List<Row> Rows { get; set; } = new List<Row>();
+    }
+
+    [XmlRoot(ElementName = "ROW")]
+    public class Row
+    {
+        [XmlElement(ElementName = "PASSTYPE")]
+        public string PassType { get; set; }
+
+        [XmlElement(ElementName = "NAME")]
+        public string Name { get; set; }
+
+        [XmlElement(ElementName = "FIRSTNAME")]
+        public string FirstName { get; set; }
+
+        [XmlElement(ElementName = "SECONDNAME")]
+        public string SecondName { get; set; }
+
+        [XmlElement(ElementName = "TABLENO")]
+        public string TableNo { get; set; }
+
+        [XmlElement(ElementName = "WDEP2")]
+        public string Department { get; set; }
+
+        [XmlElement(ElementName = "POST")]
+        public string Post { get; set; }
+
+        [XmlElement(ElementName = "DOCTYPE")]
+        public string DocType { get; set; }
+
+        [XmlElement(ElementName = "DOCSER")]
+        public string DocSer { get; set; }
+
+        [XmlElement(ElementName = "DOCNO")]
+        public string DocNo { get; set; }
+
+        [XmlElement(ElementName = "DOCISSUEORGAN")]
+        public string DocIssueOrgan { get; set; }
+
+        [XmlElement(ElementName = "ADDRESS")]
+        public string Address { get; set; }
+
+        [XmlElement(ElementName = "BIRTHPLACE")]
+        public string Birthplace { get; set; }
+
+        [XmlElement(ElementName = "SITIZENSHIP")]
+        public string Citizenship { get; set; }
+
+        [XmlElement(ElementName = "SEX")]
+        public string Sex { get; set; }
+
+        [XmlElement(ElementName = "DOCISSUEDATE")]
+        public string DocIssueDate { get; set; }
+
+        [XmlElement(ElementName = "BIRTHDATE")]
+        public string Birthdate { get; set; }
+    }
+}
